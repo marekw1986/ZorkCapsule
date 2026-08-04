490dc0118db158b272deaeaae9a08b457e103519
Marek Wiecek <marekw1986@gmail.com>
2026-08-04 11:01:07 +0200

    Clock config added

--- code/src/main.c
+++ code/src/main.c
@@ -1,10 +1,15 @@
+#include <clock.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <io.h>
+#include <compiler.h>
+#include <gclk.h>
+#include <system.h>
 #include "ztypes.h"
 
 int main(void)
 {
+	system_init();   /* configures XOSC, DFLL, GCLKs per conf_clocks.h */
 
     while (1)
     {
